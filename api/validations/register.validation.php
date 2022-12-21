9b7e4615dd393f2528cf2b08032612d07c5f96eb
Sourav Malo <sourav.cse5.bu@gmail.com>
2022-12-21 12:25:48 +0600

    read category by id api published

--- api/validations/register.validation.php
+++ api/validations/register.validation.php
@@ -28,7 +28,7 @@
   $validation->validate();
 
   if($validation->fails()) {
-    http_response_code(503);
+    http_response_code(400);
     echo json_encode(['message' => $validation->errors()->firstOfAll()[array_key_first($validation->errors()->firstOfAll())]]);
     exit();
   }
